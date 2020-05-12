b50de39fcb23becf2e3c765b98982257276156e2
leotalorac <leotalorac@unal.edu.co>
2020-05-12 08:55:21 -0500

    change name main

--- Factory/MainRestaurant.java
+++ Factory/MainRestaurant.java
@@ -1,12 +1,12 @@
 package Factory;
 
-public class main {
+public class MainRestaurant {
     public static void main(String[] args) {
         RestaurantFactoryProducer restaurantFactoryProducer = new RestaurantFactoryProducer();
         RestaurantAbstractFactory restaurantAbstractFactory1 = restaurantFactoryProducer.getRestaurantAbstractFactory(1);
         RestaurantAbstractFactory restaurantAbstractFactory2 = restaurantFactoryProducer.getRestaurantAbstractFactory(2);
         System.out.println("Soda on restaurant 1: " + restaurantAbstractFactory1.getDrink("soda"));
-        System.out.println("Soda on restaurant 2: " + restaurantAbstractFactory2.getDrink("soda"));
+        System.out.println("Beer on restaurant 2: " + restaurantAbstractFactory2.getDrink("soda"));
         System.out.println();
         System.out.println("Beer on restaurant 1: " + restaurantAbstractFactory1.getFastFood("pizza"));
         System.out.println("Beer on restaurant 2: " + restaurantAbstractFactory2.getFastFood("pizza"));
