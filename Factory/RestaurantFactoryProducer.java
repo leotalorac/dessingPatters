5302afb609e85779c260bbe279eafaa9076530b3
leotalorac <leotalorac@unal.edu.co>
2020-05-12 08:41:21 -0500

    add main and the producer

--- Factory/RestaurantFactoryProducer.java
+++ Factory/RestaurantFactoryProducer.java
@@ -4,7 +4,7 @@ public class RestaurantFactoryProducer {
     public RestaurantAbstractFactory getRestaurantAbstractFactory(int numRestaurant){
         switch(numRestaurant){
             case 1: return new Restaurant1Factory();
-            case 2: return new Restaurant1Factory();
+            case 2: return new Restaurant2Factory();
             default:return null;
         }
     }
